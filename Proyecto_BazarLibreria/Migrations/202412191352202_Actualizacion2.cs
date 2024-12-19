2465f68cc4e94fbae0a832e7f5d77d94f3e5ce14
KevObando <kevinjos608@gmail.com>
2024-12-19 07:59:13 -0600

    Prueba Ajuste en la migraciones

---
+++ Proyecto_BazarLibreria/Migrations/202412191352202_Actualizacion2.cs
@@ -0,0 +1,16 @@
+﻿namespace Proyecto_BazarLibreria.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Actualizacion2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
